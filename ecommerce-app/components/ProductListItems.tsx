77d73f32e520269b5f2b50fec70d71de4a75f2a1
Thabang Muleba <thabangrevite@gmail.com>
2026-05-31 22:15:22 +0200

    react-native, expo, TanstackQuery and cleark setup

--- ecommerce-app/components/ProductListItems.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from "react-native";
-
-export default function productListItem({ product }) {
-  return <Text style={{ fontSize: 30 }}>{product.name}</Text>;
-}
--- ecommerce-app/src/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { View, Text, FlatList } from "react-native";
-import products from "../../assets/products.json";
-
-export default function HomeScreen() {
-  return (
-    <FlatList data={products} renderItem={({ item }) => <productListItem />} />
-  );
-}
