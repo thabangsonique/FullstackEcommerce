af49be71d23a9abeec74a2cfcb8504cd1d412722
Thabang Muleba <thabangrevite@gmail.com>
2026-06-01 20:46:03 +0200

    Mobile app setup

---
+++ ecommerce-app/app/(auth)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AuthScreen = () => {
+  return (
+    <View>
+      <Text>Authentication Screen</Text>
+    </View>
+  );
+};
+
+export default AuthScreen;
--- ecommerce-app/app/_layout.tsx
+++ ecommerce-app/app/_layout.tsx
@@ -1,6 +1,22 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache"; //clerk to store user signed in session.
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+if (!publishableKey) {
+  throw new Error("Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY");
+}
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
 }
