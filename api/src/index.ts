077975a9d3d25d69f66d222bd0ea411eb917b7cd
Thabang Muleba <thabangrevite@gmail.com>
2026-06-08 17:04:10 +0200

    Fixed Render deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,7 +5,6 @@ import express, { Request, Response } from "express";
 import productRoutes from "./routes/products/index.js";
 import authRoutes from "./routes/auth/authRoutes.js";
 import ordersRoutes from "./routes/orders/ordersRoutes.js";
-import serverless from "serverless-http";
 
 const port = process.env.PORT || 3000; //port will be recieving requests and responses from
 
@@ -19,10 +18,6 @@ app.use("/auth", authRoutes);
 
 app.use("/orders", ordersRoutes);
 
-if (process.env.NODE_ENV === "dev") {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}
-
-export const handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
